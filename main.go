f059c39053a7137ae505dd808b8843439c58a4dd
zen <hasee>
2024-11-17 12:33:52 +0800

    修改逻辑为先找时间戳文件

--- main.go
+++ main.go
@@ -5,24 +5,23 @@ import (
 	"FFmpegBatchCut/util"
 	"fmt"
 	"log"
-	"os"
-	"strings"
+	"path/filepath"
 )
 
 func main() {
-	root := "F:\\原始视频\\分割完成"
+	root := "F:\\media"
 	folders, _ := util.GetFoldersWithTimestamps(root)
 	for _, folder := range folders {
 		fmt.Printf("out:=%v\n", folders)
-		mp4, _ := util.GetUniqueMP4File(folder)
-		if mp4 == "" {
-			mp4, _ = util.GetUniqueMKVFile(folder)
-			if mp4 == "" {
-				log.Printf("在 %v 目录下 mkv 和 mp4 均未找到\n", folder)
-				continue
-			}
+		timestampsFile := filepath.Join(folder, "timestamps.txt")
+		if !util.IfFileExists(timestampsFile) {
+			continue
 		}
-		timestampsFile := strings.Join([]string{folder, "timestamps.txt"}, string(os.PathSeparator))
+		videos, _ := util.GetAllVideoFilesInDir(folder)
+		if len(videos) > 1 {
+			continue
+		}
+		mp4 := videos[0]
 		timestamps := util.ReadByLine(timestampsFile)
 		timestamps = removeEmptyStrings(timestamps)
 		log.Printf("目录%v\n文件%v\n时间戳%v\n", folder, mp4, timestamps)
@@ -30,14 +29,14 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		} else {
-			if err := os.Remove(timestampsFile); err != nil {
-				log.Printf("删除%v失败\n", timestamps)
-			} else {
-				if err := os.Remove(mp4); err != nil {
-					log.Printf("删除%v失败\n", mp4)
-				}
-				log.Printf("分割文件结束,删除%v和%v失败\n", timestamps, mp4)
-			}
+			//if err := os.Remove(timestampsFile); err != nil {
+			//	log.Printf("删除%v失败\n", timestamps)
+			//} else {
+			//	if err := os.Remove(mp4); err != nil {
+			//		log.Printf("删除%v失败\n", mp4)
+			//	}
+			//	log.Printf("分割文件结束,删除%v和%v失败\n", timestamps, mp4)
+			//}
 		}
 	}
 }
