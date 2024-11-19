dcc29261656817b92aa00b0171288d3eab14e948
zen <hasee>
2024-11-19 17:16:48 +0800

    每次运行程序新建日志

--- util/log.go
+++ util/log.go
@@ -9,11 +9,17 @@ import (
 
 func SetLog(l string) {
 	// 创建一个用于写入文件的Logger实例
+
 	fileLogger := &lumberjack.Logger{
 		Filename:   l,
 		MaxSize:    1, // MB
-		MaxBackups: 3,
+		MaxBackups: 30,
 		MaxAge:     28, // days
+	}
+	//Rotate causes Logger to close the existing log file and immediately create a new one. This is a helper function for applications that want to initiate rotations outside of the normal rotation rules, such as in response to SIGHUP. After rotating, this initiates a cleanup of old log files according to the normal rules.
+	err := fileLogger.Rotate()
+	if err != nil {
+		log.Printf("Rotate error: %v", err)
 	}
 	consoleLogger := log.New(os.Stdout, "CONSOLE: ", log.LstdFlags)
 	log.SetOutput(io.MultiWriter(fileLogger, consoleLogger.Writer()))
